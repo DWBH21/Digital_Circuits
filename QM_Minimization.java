6aba7d88ec65293f70ea8602cfbf2d57f4800988
Anant <anant.maheshwary@gmail.com>
2025-09-03 00:42:23 +0530

    pi chart done

--- QM_Minimization.java
+++ QM_Minimization.java
@@ -11,10 +11,10 @@ class Minterm {
         this.s = s;
         this.mintermNos = mintermNos;
     }
-    Minterm(int num, int noVariables) {
+    Minterm(int num, int noVars) {
         StringBuilder sb = new StringBuilder();
         int tempNum = num;
-        for (int i = 1; i <= noVariables; i++) {
+        for (int i = 1; i <= noVars; i++) {
             sb.append(tempNum & 1);
             tempNum >>>= 1;
         }
@@ -78,6 +78,25 @@ class Minterm {
         newMintermNos.addAll(m.mintermNos);
         return new Minterm(sb.toString(), newMintermNos);
     }
+
+    boolean covers(int mintermNo, int noVars)
+    {
+        String s = this.s;
+        for(int i=noVars-1; i>=0; i--) 
+        {
+            char p = s.charAt(i);
+            int bit = mintermNo & 1;
+
+            mintermNo >>= 1;
+            if(p == '-')
+                continue;
+
+            p -= '0';
+            if(p != bit)   
+                return false;
+        }
+        return true;
+    }
 }
 public class QM_Minimization {
     
@@ -122,5 +141,88 @@ public class QM_Minimization {
             primeImplicants.addAll(getPrimeImplicants(nextMintermList));
             return new ArrayList<>(primeImplicants);
         }   
+    }
+    
+    static Map<Integer, List<Integer>> buildChart(List<Minterm> primeImplicants, List<Integer> ones, int noVars) 
+    {
+        Map<Integer, List<Integer>> chart = new LinkedHashMap<>();
+        for(int i=0; i<primeImplicants.size(); i++) 
+        {
+            Minterm m = primeImplicants.get(i);
+            List<Integer> covered = new ArrayList<>();
+            for(int no : ones)
+            {
+                if(m.covers(no, noVars))
+                    covered.add(no);
+            }
+            if(!covered.isEmpty())
+                chart.put(i, covered);
+        }
+        return chart;
+    }
+
+    static Set<Integer> getMinExpression(List<Minterm> primeImplicants, List<Integer> ones, int noVars) 
+    {
+        if(primeImplicants.isEmpty())
+            return new HashSet<>();
+
+        // Building the prime implicant chart
+        Map<Integer, List<Integer>> chart = buildChart(primeImplicants, ones, noVars);
+        Set<Integer> remMinterms = new HashSet<>(ones);
+
+        // Selecting esssential prime implicants
+        Set<Integer> chosenPIs = reduceByEssentials(chart, remMinterms);
+
+        // If some minterms still remain uncovered, using some trial and error
+        // if(!remMinterms.isEmpty())
+        // {
+        //     Set<Integer> cover = findMinCover(chart, remMinterms);
+        //     chosenPIs.addAll(cover);
+        // }        
+        
+        return chosenPIs;
+    }
+
+    private static Set<Integer> findMinCover(Map<Integer, List<Integer>> chart, Set<Integer> remMinterms) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findMinCover'");
+    }
+
+    static Set<Integer> reduceByEssentials(Map<Integer, List<Integer>> chart, Set<Integer> remMinterms)
+    {
+        Set<Integer> chosenImplicants = new HashSet<>();
+        boolean progress = true;
+        while(progress)   // while there is progress
+        {
+            progress = false;
+            
+            Map<Integer, List<Integer>> reverseMap = new HashMap<>();   // constructs the map from remTerms to the List of prime implicants it is a part of 
+            for(int minterm : remMinterms) 
+            {
+                reverseMap.put(minterm, new ArrayList<>());
+            }
+            for(Map.Entry<Integer, List<Integer>> entry : chart.entrySet()) 
+            {
+                for(int minterm : entry.getValue()) 
+                {
+                    if(remMinterms.contains(minterm))
+                        reverseMap.get(minterm).add(entry.getKey());
+                }
+            }
+
+            for(Map.Entry<Integer, List<Integer>> entry : reverseMap.entrySet())
+            {
+                if(entry.getValue().size() == 1) // if the minterm is covered by only one prime implicant
+                {
+                    int essential = entry.getValue().get(0);
+                    if(chosenImplicants.add(essential))
+                    {
+                        progress = true;
+                        remMinterms.removeAll(chart.get(essential));
+                    }
+                }
+            }
+        }
+        return chosenImplicants;
     }
 }
